6beb8780daebbfd1df32081ccf7068b4d566d2ab
Mostafa Desoky <mostafadesoky95@gmail.com>
2020-08-13 02:51:06 +0200

    Adding auto generation ID and list of Owners and Vets.

--- pet-clinic-data/src/main/java/com/mostafa/springpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/mostafa/springpetclinic/services/map/VetServiceMap.java
@@ -2,9 +2,11 @@ package com.mostafa.springpetclinic.services.map;
 
 import com.mostafa.springpetclinic.model.Vet;
 import com.mostafa.springpetclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
@@ -23,7 +25,7 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
-        return super.save(object.getId(), object);
+        return super.save(object);
     }
 
     @Override
--- pet-clinic-web/src/main/java/com/mostafa/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/mostafa/springpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.mostafa.springpetclinic.model.Owner;
 import com.mostafa.springpetclinic.model.Vet;
 import com.mostafa.springpetclinic.services.OwnerService;
 import com.mostafa.springpetclinic.services.VetService;
-import com.mostafa.springpetclinic.services.map.OwnerServiceMap;
-import com.mostafa.springpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,42 +14,38 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
 
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        Owner owner1 = new  Owner();
-        owner1.setId(1L);
+        Owner owner1 = new Owner();
         owner1.setFirstName("Mostafa");
         owner1.setLastName("Desoky");
 
         ownerService.save(owner1);
 
 
-        Owner owner2 = new  Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Ahmed");
-        owner1.setLastName("Mohamed");
+        Owner owner2 = new Owner();
+        owner2.setFirstName("Ahmed");
+        owner2.setLastName("Mohamed");
 
         ownerService.save(owner2);
 
         System.out.println("loaded owners");
 
         Vet vet1 = new  Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Shawky");
         vet1.setLastName("Rabie");
 
         vetService.save(vet1);
 
         Vet vet2 = new  Vet();
-        vet1.setId(2L);
-        vet1.setFirstName("Rady");
-        vet1.setLastName("Esmail");
+        vet2.setFirstName("Rady");
+        vet2.setLastName("Esmail");
 
         vetService.save(vet2);
 
